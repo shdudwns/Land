e6f18bc5ad56848a891a9ce204ecdd3109165191
jun04 <jun040329@gmail.com>
2025-02-14 15:49:14 +0900

    Update Main.php

--- src/HybridIslandPlugin/Main.php
+++ src/HybridIslandPlugin/Main.php
@@ -3,36 +3,49 @@
 namespace HybridIslandPlugin;
 
 use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
 use HybridIslandPlugin\config\ConfigManager;
+use HybridIslandPlugin\world\WorldManager;
 use HybridIslandPlugin\command\IslandCommand;
 use HybridIslandPlugin\command\GridLandCommand;
 use HybridIslandPlugin\command\SkyBlockCommand;
+use HybridIslandPlugin\generator\GridLandGenerator;
+use HybridIslandPlugin\generator\SkyBlockGenerator;
 
-class Main extends PluginBase {
+class Main extends PluginBase implements Listener {
 
     private static Main $instance;
+    private ConfigManager $configManager;
+    private WorldManager $worldManager;
 
     public static function getInstance(): Main {
         return self::$instance;
     }
 
-    protected function onEnable(): void {
+    public function onEnable(): void {
         self::$instance = $this;
 
-        // ✅ ConfigManager 초기화
         ConfigManager::init();
+        
+        $this->configManager = new ConfigManager($this);
+        $this->worldManager = new WorldManager($this);
 
-        // ✅ 명령어 등록
-        $this->getServer()->getCommandMap()->registerAll("HybridIslandPlugin", [
-            new IslandCommand(),
-            new GridLandCommand(),
-            new SkyBlockCommand()
+        $this->getServer()->getCommandMap()->registerAll("hybridisland", [
+            new IslandCommand($this),
+            new GridLandCommand($this),
+            new SkyBlockCommand($this)
         ]);
 
-        $this->getLogger()->info("HybridIslandPlugin 활성화 완료!");
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+
+        $this->getLogger()->info("HybridIslandPlugin 활성화 완료");
     }
 
-    protected function onDisable(): void {
-        $this->getLogger()->info("HybridIslandPlugin 비활성화 완료!");
+    public function getConfigManager(): ConfigManager {
+        return $this->configManager;
+    }
+
+    public function getWorldManager(): WorldManager {
+        return $this->worldManager;
     }
 }
