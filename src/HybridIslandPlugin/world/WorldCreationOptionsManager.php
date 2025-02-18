6f58409e1604c38fb32fec3ccfff4d47cfa4e31c
jun04 <jun040329@gmail.com>
2025-02-18 17:10:04 +0900

    Update WorldCreationOptionsManager.php

--- src/HybridIslandPlugin/world/WorldCreationOptionsManager.php
+++ src/HybridIslandPlugin/world/WorldCreationOptionsManager.php
@@ -12,24 +12,22 @@ class WorldCreationOptionsManager {
 
     public static function getOptions(string $type): WorldCreationOptions {
         $options = new WorldCreationOptions();
-        
+        $options->setSeed(mt_rand());
+
         switch (strtolower($type)) {
             case "island":
-                $seed = mt_rand();
-                $options->setGeneratorClass(IslandGenerator::class); // ✅ 클래스 이름(string)으로 수정
-                $options->setSeed($seed); // ✅ 랜덤 시드 적용
+                $options->setGeneratorClass(IslandGenerator::class);
+                $options->setSpawnLocation(new Vector3(8, 65, 8));
                 break;
 
             case "gridland":
-                $options->setGeneratorClass(GridLandGenerator::class); // ✅ 수정
+                $options->setGeneratorClass(GridLandGenerator::class);
                 $options->setSpawnLocation(new Vector3(0, 65, 0));
-                $options->setSeed(mt_rand());
                 break;
 
             case "skyblock":
-                $options->setGeneratorClass(SkyBlockGenerator::class); // ✅ 수정
+                $options->setGeneratorClass(SkyBlockGenerator::class);
                 $options->setSpawnLocation(new Vector3(8, 65, 8));
-                $options->setSeed(mt_rand());
                 break;
 
             default:
